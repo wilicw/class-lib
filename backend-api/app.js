0a7de5fce6e93a0f3517fa39414cc34e174cf2c7
William Chang <wilicw1346@gmail.com>
2021-05-06 07:57:15 +0800

    vue + express integration

--- backend-api/app.js
+++ backend-api/app.js
@@ -1,10 +1,9 @@
-var createError = require('http-errors');
+const history = require('connect-history-api-fallback')
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var booksRouter = require('./routes/books');
 var usersRouter = require('./routes/users');
 
@@ -16,10 +15,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/books', booksRouter);
+// serve your css as static
+const static_dir = path.join(__dirname, "/../front-ui/dist")
+app.use(history())
+app.use(express.static(static_dir));
+app.get("/", (req, res) => {
+  res.render(path.join(static_dir, "/index.html"));
+});
+app.use('/api/users', usersRouter);
+app.use('/api/books', booksRouter);
 
 
 // error handler
--- front-ui/config.js
+++ front-ui/config.js
@@ -1,6 +1,6 @@
 import { publicPath } from "./vue.config"
 const config = {
-    api: process.env.NODE_ENV == 'production' ? `${publicPath}api` : 'http://127.0.0.1:3000',
+    api: process.env.NODE_ENV == 'production' ? `${publicPath}api` : 'http://127.0.0.1:3000/api',
 }
 export { config }
 export default {}
